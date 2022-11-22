7c8df3fb35ae815222718403499ec003feb3a53f
sasa5680 <sasa5680@naver.com>
2022-11-22 21:06:27 +0900

    Feign log 설정

--- src/main/java/com/example/feign/config/FeignConfig.java
+++ src/main/java/com/example/feign/config/FeignConfig.java
@@ -1,8 +1,17 @@
 package com.example.feign.config;
 
+import com.example.feign.feign.logger.FeignCustomLogger;
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
+
+    //global 설정으로 만든다.
+    @Bean
+    public Logger feinLogger() {
+        return new FeignCustomLogger();
+    }
 
 }
